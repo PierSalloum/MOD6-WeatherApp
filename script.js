5428db141b1427d4bb6e896459e52deded0838c1
Pier Salloum <piersalloum@gmail.com>
2023-04-12 22:11:02 -0700

    working display. working search history with clear local storage option

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
+
 const getLocation = async () => {
     const inputCity = document.getElementById("input-city").value
-    console.log(inputCity)
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=2e57c835085c11b3a7f95dbb976b3da7`)
-    console.log(response)
     const data = await response.json()
-    console.log(data)
     getWeather(data[0].lon, data[0].lat)
     let lastSearch = document.querySelector(".list")
-    let lastSearchContent = document.createElement("li")
+    let lastSearchContent = document.createElement("button")
     lastSearchContent.className += ".list-item"
     lastSearchContent.innerHTML = inputCity
+    lastSearchContent.addEventListener("click", handleHistory)
     lastSearch.appendChild(lastSearchContent);
-    data.innerHTML = '';
+    localStorage.setItem("lastSearchResult", lastSearch.innerHTML);
+    document.querySelector(".forecast-container").innerHTML = '';
 }
 
+
+
+async function handleHistory (event) {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${event.target.innerHTML}&limit=1&appid=2e57c835085c11b3a7f95dbb976b3da7`)
+    const data = await response.json()
+    getWeather(data[0].lon, data[0].lat)
+    document.querySelector(".forecast-container").innerHTML = '';
+}
+const lastSearchResult = localStorage.getItem("lastSearchResult");
+if (lastSearchResult) {
+    document.querySelector(".list").innerHTML = lastSearchResult;
+}
+
+const clearHistory = document.getElementById("clear-history");
+clearHistory.addEventListener("click", () => {
+    localStorage.removeItem("lastSearchResult");
+    document.querySelector(".list").innerHTML = "";
+
+});
 const buttonCity = document.getElementById("button-city")
 buttonCity.addEventListener("click", getLocation)
 
